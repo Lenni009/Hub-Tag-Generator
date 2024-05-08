58a46853407ce8dd190db67f4e3408e883992f72
Lenni009 <guentherhanslphdyt@gmail.com>
2024-05-09 00:00:15 +0200

    add prefix generator tests

--- test/components/prefixGenerator.test.ts
+++ test/components/prefixGenerator.test.ts
@@ -2,6 +2,8 @@ import PrefixGenerator from '@/components/PrefixGenerator.vue';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { mount, type VueWrapper } from '@vue/test-utils';
 import { createTestingPinia } from '@pinia/testing';
+import { usePrefixDataStore } from '@/stores/prefixData';
+import { storeToRefs } from 'pinia';
 
 describe('PrefixGenerator', () => {
   let wrapper: VueWrapper;
@@ -17,5 +19,32 @@ describe('PrefixGenerator', () => {
 
   it('should render correctly', () => {
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should reset data on button click', async () => {
+    const store = usePrefixDataStore(pinia);
+    const { glyphs, systemName } = storeToRefs(store);
+    glyphs.value = '0001A21117FF';
+    systemName.value = 'Hello World';
+    const button = wrapper.find('button.reset-button');
+    await button.trigger('click');
+
+    expect(glyphs.value + systemName.value).toBe('');
+  });
+
+  it('should have the correct label', async () => {
+    const output = wrapper.find('.output-label');
+
+    expect(output.element.textContent).toBe('Prefix:');
+  });
+
+  it('should have the correct label', async () => {
+    const systemInput = wrapper.find('#systemName');
+    const glyphInput = wrapper.find('#portalglyphsInput');
+    const output = wrapper.find('.prefix-output');
+    await systemInput.setValue('Hello World');
+    await glyphInput.setValue('0001A21117FF');
+
+    expect(output.element.textContent).toBe('EV1-1 Hello World');
   });
 });
